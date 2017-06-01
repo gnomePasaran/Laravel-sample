d643f4dfcf5c56d2b3338e778aa4d1e05a0ef825
Alexander Ivahnenko <alexander_i@fast-report.com>
2017-06-02 00:19:54 +0300

    change route for Post

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', ['as' => 'posts', 'uses' => 'PostController@index']);
