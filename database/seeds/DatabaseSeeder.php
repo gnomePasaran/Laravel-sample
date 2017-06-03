bb57b251945e097e638f57acfa0c589971ee2cb4
Alexander Ivahnenko <alexander_i@fast-report.com>
2017-06-03 16:39:08 +0300

    add Seeds for Posts

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,41 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(PostsSeeder::class);
+    }
+}
+
+class PostsSeeder extends Seeder
+{
+
+    public function run()
+    {
+      DB::table('posts')->delete();
+      Post::create([
+        'title' => 'First-post',
+        'slug' => 'first-post',
+        'excerpt' => '<b>First Post body</b>',
+        'content' => '<b>Content First Post body</b>',
+        'published' => true,
+        'published_at' => DB::raw('CURRENT_TIMESTAMP'),
+      ]);
+
+      Post::create([
+        'title' => 'Second-post',
+        'slug' => 'second-post',
+        'excerpt' => '<b>Second Post body</b>',
+        'content' => '<b>Second First Post body</b>',
+        'published' => false,
+        'published_at' => DB::raw('CURRENT_TIMESTAMP'),
+      ]);
+
+      Post::create([
+        'title' => 'Third-post',
+        'slug' => 'third-post',
+        'excerpt' => '<b>Third Post body</b>',
+        'content' => '<b>Third First Post body</b>',
+        'published' => true,
+        'published_at' => DB::raw('CURRENT_TIMESTAMP'),
+      ]);
     }
 }
