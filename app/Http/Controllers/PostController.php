c6413cb1bf5bec72cd52ab6af7b731dea4348634
Alexander Ivahnenko <alexander_i@fast-report.com>
2017-06-10 18:57:59 +0300

    add Post actions

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -18,5 +18,42 @@ class PostController extends Controller
         return view('posts.index', ['posts' => $posts]);
     }
 
-    
+    public function create()
+    {
+        $post = new Post;
+        return view('posts.create', ['post' => $post]);
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+          'title' => 'required|unique:posts|min:5|max:50',
+          'content' => 'required'
+        ]);
+        $setPost = $request->only('title', 'content');
+
+        Post::create($setPost); // returns array
+        return redirect()->route('posts');
+    }
+
+    public function edit($id)
+    {
+        $post = Post::find($id);
+        return view('posts.edit', ['post' => $post]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+          'title' => 'required|unique:posts|min:5|max:50',
+          'content' => 'required'
+        ]);
+        $setPost = $request->only('title', 'content', 'published');
+        $post = Post::find($id);
+
+        if ($post->save($setPost)) // returns true/false
+          return redirect()->route('posts');
+        else
+          return view('posts.create', ['post' => $post]);
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,6 +7,23 @@ use Carbon\Carbon;
 
 class Post extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['title', 'content'];
+
+    protected static function boot()
+    {
+        static::saving(function($model) {
+          $model->slug = Post::seoUrl($model->title);
+          $model->excerpt = substr($model->content, 0, 150);
+          if ($model->published == true)
+            $model->published_at = Carbon::now()->toDateTimeString();
+        });
+    }
+
     public function getPublishedPosts()
     {
         return $this->latest('published_at')->published()->get();
@@ -16,5 +33,18 @@ class Post extends Model
     {
         $query->where('published_at', '<=', Carbon::now())
               ->where('published', '=', true);
+    }
+
+    private static function seoUrl($string)
+    {
+        //Lower case everything
+        $string = strtolower($string);
+        //Make alphanumeric (removes all other characters)
+        $string = preg_replace("/[^a-z0-9_\s-]/", "", $string);
+        //Clean up multiple dashes or whitespaces
+        $string = preg_replace("/[\s-]+/", " ", $string);
+        //Convert whitespaces and underscore to dash
+        $string = preg_replace("/[\s_]/", "-", $string);
+        return $string;
     }
 }
