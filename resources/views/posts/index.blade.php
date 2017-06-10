a616ad8da74bde2f799d99b9e0568e3849cf626b
Alexander Ivahnenko <alexander_i@fast-report.com>
2017-06-10 19:25:34 +0300

    add Post views

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,13 +1,12 @@
 @extends('app')
 
-
 @section('content')
-  {{ link_to_route('posts', 'published') }} &nbsp;&nbsp;&nbsp; {{ link_to_route('post.create', 'new_post') }}
+  {{ link_to_route('posts', 'published') }} &nbsp;&nbsp;&nbsp; {{ link_to_route('post.create', 'New post') }}
 
   @foreach ($posts as $post)
     <article class="">
-      <h2>{!! $post->title !!}</h2>
-      <p>{!! $post->excerpt !!}</p>
+      <h2><b>{{ $post->title }}</b> ({{ link_to_route('post.edit', 'Edit post', $post->id) }})</h2>
+      <p><b>{{ $post->excerpt }}</b></p>
       <p>Published: {{ $post->published_at }}</p>
     </article>
   @endforeach
