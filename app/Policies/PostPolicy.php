da80879f96180946cf913fe1530d457f8b75ec2b
Alexander Ivahnenko <alexander_i@fast-report.com>
2017-06-12 23:23:42 +0300

    add PostPolicy

---
+++ app/Policies/PostPolicy.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\Post;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class PostPolicy extends AppPolicy
+{
+    public function update(User $user, Post $post)
+    {
+        return $post->user_id === $user->id;
+    }
+
+    public function edit(User $user, Post $post)
+    {
+        $this->update($user, $post);
+    }
+
+    public function destroy(User $user, Post $post)
+    {
+        $this->update($user, $post);
+    }
+}
