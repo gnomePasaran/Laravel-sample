78f02a12e1cd36620e54387d6e364e39b2aa671f
Alexander Ivahnenko <alexander_i@fast-report.com>
2017-06-18 17:43:12 +0300

    add releateions between Post, Answer and User

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -4,13 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Post;
+use App\Models\User;
 
 class Answer extends Model
 {
-    protected $fillable = ['content', 'user_id']; 
+    protected $fillable = ['content', 'user_id'];
 
     public function post()
     {
-        return $this->belongsTo('Post');
+        return $this->belongsTo(Post::class);
     }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
