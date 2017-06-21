39d4f24eb8a46585255464f8990d3e19002b4b89
Alexander Ivahnenko <alexander_i@fast-report.com>
2017-06-22 01:21:22 +0300

    add routes for vote actions

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,36 @@ Route::resource('post.answer', 'AnswerController', [
 
 Route::get('answer/{id}/toggle_best', [
   'as' => 'answer.toggle_best',
-  'uses' => 'AnswerController@toggle_best'
+  'uses' => 'AnswerController@toggleBest'
+]);
+
+Route::post('post/{id}/vote_up', [
+  'as' => 'post.vote_up',
+  'uses' => 'PostController@voteUp'
+]);
+
+Route::post('post/{id}/vote_down', [
+  'as' => 'post.vote_down',
+  'uses' => 'PostController@voteDown'
+]);
+
+Route::post('post/{id}/vote_cancel', [
+  'as' => 'post.vote_cancel',
+  'uses' => 'PostController@voteCancel'
+]);
+
+
+Route::post('answer/{id}/vote_up', [
+  'as' => 'answer.vote_up',
+  'uses' => 'AnswerController@voteUp'
+]);
+
+Route::post('answer/{id}/vote_down', [
+  'as' => 'answer.vote_down',
+  'uses' => 'AnswerController@voteDown'
+]);
+
+Route::post('answer/{id}/vote_cancel', [
+  'as' => 'answer.vote_cancel',
+  'uses' => 'AnswerController@voteCancel'
 ]);
