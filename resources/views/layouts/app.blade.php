e4f41fc3aefffb6f100459e57c1fb8659a5ff0bc
Alexander Ivahnenko <alexander_i@fast-report.com>
2018-03-29 23:10:48 +0300

    add views for about us

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,6 +31,9 @@
                     <a class="navbar-brand" href="{{ url('/') }}">
                         {{ config('app.name', 'Laravel') }}
                     </a>
+                    <a class="navbar-brand" href="{{ route('about-us') }}">
+                        About us
+                    </a>
                 </div>
 
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
