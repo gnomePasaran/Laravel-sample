246379c7865b8f6989debf740478dcb372158c58
Alexander Ivakhnenko <alex-rolling@yandex.ru>
2018-07-13 22:06:06 +0300

    update Laravel & packeges

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -20,12 +20,10 @@ use App\Models\Vote;
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 $factory->define(User::class, function (Faker\Generator $faker) {
-    static $password;
-
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
+        'password' => bcrypt('secret'),
         'remember_token' => str_random(10),
     ];
 });
